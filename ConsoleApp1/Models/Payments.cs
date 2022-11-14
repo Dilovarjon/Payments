3bf2057da510e94a4ba41a7cb6d3c4723f7f36a5
dilovar-zero <dilovarzoidboev@gmail.com>
2022-11-14 19:51:16 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Models/Payments.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Models
+{
+    public class Payments
+    {
+        public Guid Id { get; set; }
+        public decimal UserPayments { get; set; }
+        public Guid ComputerId { get; set; }
+        public Computer? Computer{ get; set; }
+
+        public Guid TelephoneId { get; set; }
+        public Telephone? Telephone{ get; set; }
+        
+        public Guid TelevisionId { get; set; }
+        public Television? Television{ get; set; }
+
+    }
+}
---
+++ ConsoleApp1/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string phoneNumber { get; set; }
+        
+        public Guid PaymentsId { get; set; }
+        public Payments Payments { get; set; }
+    }
+}
